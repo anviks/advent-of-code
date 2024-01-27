d746b71ed688c1a9b15901307c2372e69e1e5b1f
anviks <anviks@ttu.ee>
2024-01-27 14:57:39 +0200

    Solved day 16.

---
+++ 2023/day_16/solution.py
@@ -0,0 +1,97 @@
+from __future__ import annotations
+
+from enum import Enum
+
+from utils_anviks.decorators import read_data, stopwatch
+
+
+class Direction(Enum):
+    UP = (-1, 0)
+    DOWN = (1, 0)
+    RIGHT = (0, 1)
+    LEFT = (0, -1)
+
+    def apply(self, i: int, j: int) -> tuple[int, int]:
+        di, dj = self.value
+        return i + di, j + dj
+
+    def apply_backslash(self) -> Direction:
+        return {
+            self.RIGHT: self.DOWN,
+            self.DOWN: self.RIGHT,
+            self.LEFT: self.UP,
+            self.UP: self.LEFT,
+        }[self]
+
+    def apply_slash(self) -> Direction:
+        return {
+            self.RIGHT: self.UP,
+            self.UP: self.RIGHT,
+            self.LEFT: self.DOWN,
+            self.DOWN: self.LEFT,
+        }[self]
+
+
+def get_energized_tile_count(tiles: list[list[str]], direction: Direction, i: int, j: int) -> int:
+    tiles = walk(tiles, direction, i, j)
+    tile_coords = {(t[1], t[2]) for t in tiles}
+
+    return len(tile_coords)
+
+
+@read_data('data.txt', sep2='')
+@stopwatch
+def solution(data: list[list[str]], part: int):
+    max_tiles = 0
+
+    if part == 1:
+        max_tiles = get_energized_tile_count(data, Direction.RIGHT, 0, 0)
+    else:
+        for i in range(len(data)):
+            max_tiles = max(max_tiles, get_energized_tile_count(data, Direction.RIGHT, i, 0))
+            max_tiles = max(max_tiles, get_energized_tile_count(data, Direction.LEFT, i, len(data[0]) - 1))
+
+        for j in range(len(data[0])):
+            max_tiles = max(max_tiles, get_energized_tile_count(data, Direction.DOWN, 0, j))
+            max_tiles = max(max_tiles, get_energized_tile_count(data, Direction.UP, len(data) - 1, j))
+
+    with open('output.txt', 'w') as f:
+        f.write('\n'.join(''.join(row) for row in data))
+
+    return max_tiles
+
+
+def walk(tile_matrix: list[list[str]], direction: Direction, i: int = 0, j: int = 0, tiles: set = None) -> set:
+    if tiles is None:
+        tiles = set()
+    tiles.add((direction, i, j))
+
+    while True:
+        match tile_matrix[i][j]:
+            case '\\':
+                direction = direction.apply_backslash()
+            case '/':
+                direction = direction.apply_slash()
+            case '-':
+                if direction in (Direction.DOWN, Direction.UP):
+                    tiles.update(walk(tile_matrix, Direction.LEFT, i, j, tiles))
+                    direction = Direction.RIGHT
+            case '|':
+                if direction in (Direction.LEFT, Direction.RIGHT):
+                    tiles.update(walk(tile_matrix, Direction.UP, i, j, tiles))
+                    direction = Direction.DOWN
+
+        i, j = direction.apply(i, j)
+
+        if (i < 0 or j < 0 or i >= len(tile_matrix) or j >= len(tile_matrix[0])
+                or (direction, i, j) in tiles):
+            break
+
+        tiles.add((direction, i, j))
+
+    return tiles
+
+
+if __name__ == '__main__':
+    print(solution(1))  # 7242
+    print(solution(2))  # 7572
