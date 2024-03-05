5d9d636e8fdeb5ee6ddc99d7de51eef2dbfac16a
anviks <anviks@ttu.ee>
2024-03-05 23:45:31 +0200

    Add example file creation.

--- edit_files.py
+++ edit_files.py
@@ -2,19 +2,25 @@
 
 import os
 
+overwrite_solution = False
+
 for year in range(2023, 2024):
     if not os.path.exists(str(year)):
         os.mkdir(str(year))
-    for day in range(10, 26):
+    for day in range(21, 25):
         day_folder = f"{year}/day_{day}"
-        data_file = f"{day_folder}/data.txt"
-        solution_file = f"{day_folder}/solution.py"
+        data_file = day_folder + "/data.txt"
+        example_file = day_folder + "/example.txt"
+        solution_file = day_folder + "/solution.py"
         template_file = "template.txt"
 
         if not os.path.exists(day_folder):
             os.mkdir(day_folder)
 
-        with open(data_file, "a") as a, open(solution_file, "a") as b:
+        # Ensure that the files exist without overwriting them.
+        with open(data_file, 'a'), open(solution_file, 'a'), open(example_file, 'a'):
             pass
-        with open(template_file) as template, open(solution_file, "w") as solution:
-            solution.write(template.read())
+
+        if overwrite_solution:
+            with open(template_file, 'r') as template, open(solution_file, 'w') as solution:
+                solution.write(template.read())
