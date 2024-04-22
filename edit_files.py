a29a7b4a4add82f05e354b4f214ed25dfdce8b7e
anviks <anviks@ttu.ee>
2024-04-22 14:57:51 +0300

    Use os.makedirs instead of error-raising os.mkdir.

--- edit_files.py
+++ edit_files.py
@@ -4,18 +4,15 @@ import os
 
 overwrite_solution = False
 
-for year in range(2023, 2024):
-    if not os.path.exists(str(year)):
-        os.mkdir(str(year))
-    for day in range(21, 25):
+for year in range(2021, 2022):
+    for day in range(10, 25):
         day_folder = f"{year}/day_{day}"
         data_file = day_folder + "/data.txt"
         example_file = day_folder + "/example.txt"
         solution_file = day_folder + "/solution.py"
         template_file = "template.txt"
 
-        if not os.path.exists(day_folder):
-            os.mkdir(day_folder)
+        os.makedirs(day_folder, exist_ok=True)
 
         # Ensure that the files exist without overwriting them.
         with open(data_file, 'a'), open(solution_file, 'a'), open(example_file, 'a'):
