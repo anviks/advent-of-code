7518c2fc296e81fd3245ca8af53342acd01a1a1d
anviks <anviks@ttu.ee>
2024-12-04 23:11:25 +0200

    Organise code better and move file/directory creations to the same file with input data getter

--- scrape.py
+++ scrape.py
@@ -1,62 +1,121 @@
-import json
+import os
 import re
 
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 from html2text import html2text
 from utils_anviks import b64decode
+from datetime import datetime as dt
 
-login_url = "https://github.com/session"
-session = requests.Session()
-session.headers["User-Agent"] = "Private non-automated script by andreasviks0@gmail.com"
-req = session.get(login_url).text
-html = BeautifulSoup(req, features="lxml")
-token = html.find("input", {"name": "authenticity_token"}).attrs['value']
+load_dotenv()
 
-with open("login.json") as f:
-    creds: dict = json.load(f)
+LOGIN_URL = 'https://github.com/session'
+EMAIL = os.getenv('EMAIL')
 
-payload = {
-    "login": creds.get("email"),
-    "password": b64decode(creds.get("password"), 20),
-    "authenticity_token": token
-}
 
-# POST request to the login page
-response = session.post(login_url, data=payload)
-print(response.status_code)
+def get_int_input(prompt: str, default: int) -> int:
+    while True:
+        try:
+            return int(input(prompt) or default)
+        except ValueError:
+            print('Invalid input. Please enter a number.')
 
-# AOC auth page.
-session.get("https://adventofcode.com/auth/github")
 
-for year in range(2023, 2024):
-    for day in range(16, 24):
-        dir_ = f"{year}/day_{day}/"
+def get_bool_input(prompt: str) -> bool:
+    while True:
+        answer = input(prompt).lower()
+        if answer in ('y', 'yes'):
+            return True
+        elif answer in ('n', 'no'):
+            return False
+        print('Invalid input. Please enter y/yes or n/no.')
 
-        personal_data = session.get(f"https://adventofcode.com/{year}/day/{day}/input")
-        task_description = session.get(f"https://adventofcode.com/{year}/day/{day}")
 
-        with open(dir_ + "data.txt", "w") as f:
-            f.write(personal_data.text.removesuffix("\n"))
+def create_files(*, overwrite_solution: bool):
+    for year in range(start_year, start_year + years_count):
+        for day in range(start_day, start_day + days_count):
+            day_folder = f"{year}/day_{day}"
+            data_file = day_folder + "/data.txt"
+            example_file = day_folder + "/example.txt"
+            solution_file = day_folder + "/solution.py"
+            template_file = "template.txt"
 
-        pretty_description = html2text(task_description.text)
+            os.makedirs(day_folder, exist_ok=True)
 
-        # Replace homepage link with a link to current day's task.
-        pretty_description = re.sub(r"(?<=# \[Advent of Code]\()/(?=\))",
-                                    f"https://adventofcode.com/{year}/day/{day}",
-                                    pretty_description)
+            # Ensure that the files exist without overwriting them.
+            with open(data_file, 'a'), open(solution_file, 'a'), open(example_file, 'a'):
+                pass
 
-        # Replace root-relative links with absolute links.
-        pretty_description = re.sub(r"(\[.*]\()(/.*\))",
-                                    r"\1https://adventofcode.com\2",
-                                    pretty_description)
+            if overwrite_solution:
+                with open(template_file, 'r') as template, open(solution_file, 'w') as solution:
+                    solution.write(template.read())
 
-        # Replace relative input link with absolute link.
-        pretty_description = re.sub(r"(\[get your puzzle input]\()(\d{1,2}/input\))",
-                                    fr"\1https://adventofcode.com/{year}/day/\2",
-                                    pretty_description)
 
-        with open(dir_ + "task_description.md", "w", encoding='utf-8') as f:
-            f.write(pretty_description)
+def get_input_data():
+    session = requests.Session()
+    session.headers['User-Agent'] = f'Private non-automated script by {EMAIL}'
+    req = session.get(LOGIN_URL).text
+    html = BeautifulSoup(req, features='html.parser')
+    token = html.find('input', {'name': 'authenticity_token'}).attrs['value']
 
-session.close()
+    payload = {
+        'login': EMAIL,
+        'password': b64decode(os.getenv('PASSWORD'), 20),
+        'authenticity_token': token
+    }
+
+    # POST request to the login page
+    response = session.post(LOGIN_URL, data=payload)
+    response.raise_for_status()
+
+    # AOC auth page.
+    session.get('https://adventofcode.com/auth/github')
+
+    for year in range(start_year, start_year + years_count):
+        for day in range(start_day, start_day + days_count):
+            dir_ = f'{year}/day_{day}/'
+
+            personal_data = session.get(f'https://adventofcode.com/{year}/day/{day}/input')
+            task_description = session.get(f'https://adventofcode.com/{year}/day/{day}')
+
+            with open(dir_ + 'data.txt', 'w') as f:
+                f.write(personal_data.text.removesuffix('\n'))
+
+            pretty_description = html2text(task_description.text)
+
+            # Replace homepage link with a link to current day's task.
+            pretty_description = re.sub(r'(?<=# \[Advent of Code]\()/(?=\))',
+                                        f'https://adventofcode.com/{year}/day/{day}',
+                                        pretty_description)
+
+            # Replace root-relative links with absolute links.
+            pretty_description = re.sub(r'(\[.*]\()(/.*\))',
+                                        r'\1https://adventofcode.com\2',
+                                        pretty_description)
+
+            # Replace relative input link with absolute link.
+            pretty_description = re.sub(r'(\[get your puzzle input]\()(\d{1,2}/input\))',
+                                        fr'\1https://adventofcode.com/{year}/day/\2',
+                                        pretty_description)
+
+            with open(dir_ + 'task_description.md', 'w', encoding='utf-8') as f:
+                f.write(pretty_description)
+
+    session.close()
+
+
+now = dt.now()
+day_default = now.day if now.month == 12 and now.day <= 25 else -1
+
+start_year = get_int_input('Start year: ', now.year)
+years_count = get_int_input('Years count: ', 1)
+start_day = get_int_input('Start day: ', day_default)
+days_count = get_int_input('Days count: ', 1)
+
+if start_day == -1:
+    raise ValueError('Invalid day.')
+
+create_files(overwrite_solution=get_bool_input('Do you want to overwrite the solution files? (y/n): '))
+if get_bool_input('Do you want to fetch the input data? (y/n): '):
+    get_input_data()
