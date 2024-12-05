39d0e0e7fbfe88970146a7f514422bab0fce73e9
anviks <anviks@ttu.ee>
2024-12-05 11:17:52 +0200

    Solve 2024 day 5 part 1

---
+++ 2024/day_5/solution.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+
+from utils_anviks import parse_file_content, parse_string, stopwatch
+
+file = 'data.txt'
+file0 = 'example.txt'
+data = parse_file_content(file, ('\n\n',), str)
+rules, updates = data
+rules = parse_string(rules, ('\n', '|'), int)
+updates = parse_string(updates, ('\n', ','), int)
+
+rules_map = defaultdict(list)
+for rule in rules:
+    rules_map[rule[0]].append(rule[1])
+
+
+def part1():
+    acc = 0
+
+    for update in updates:
+        for i, n in enumerate(update):
+            following_nums = rules_map[n]
+            for fnum in following_nums:
+                if fnum in update[:i]:
+                    break
+            else:
+                continue
+            break
+        else:
+            acc += update[len(update) // 2]
+
+    return acc
+
+
+def part2():
+    pass
+
+
+if __name__ == '__main__':
+    print(part1())
+    print(part2())
