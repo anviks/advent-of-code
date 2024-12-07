af282cbbac658335c5003233b084727141222979
anviks <anviks@ttu.ee>
2024-12-07 17:45:22 +0200

    Make day 7 solution shorter

--- 2024/day_7/solution.py
+++ 2024/day_7/solution.py
@@ -8,28 +8,20 @@ data = parse_file_content(file, ('\n', ': ', ' '), int)
 def is_computable(value: int, operands: list[int], concat: bool) -> bool:
     *ops, num = operands
 
-    if not ops:
-        return value == num
-
-    if value % num == 0 and is_computable(value // num, ops, concat):
-        return True
-
-    if is_computable(value - num, ops, concat):
-        return True
-
-    if concat and str(value).endswith(str(num)) and is_computable(value // 10 ** len(str(num)), ops, concat):
-        return True
-
-    return False
+    return any((
+        value % num == 0 and is_computable(value // num, ops, concat),
+        is_computable(value - num, ops, concat),
+        concat and str(value).endswith(str(num)) and is_computable(value // 10 ** len(str(num)), ops, concat),
+    )) if ops else value == num
 
 
 @stopwatch
 def solution(part: int):
-    acc = 0
-    for value, operands in data:
-        if is_computable(value[0], operands, concat=part == 2):
-            acc += value[0]
-    return acc
+    return sum(
+        value[0]
+        for value, operands in data
+        if is_computable(value[0], operands, concat=part == 2)
+    )
 
 
 if __name__ == '__main__':
