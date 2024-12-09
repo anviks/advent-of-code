ea154941b1f003158f535f0eb743248df8367490
anviks <anviks@ttu.ee>
2024-12-09 17:05:04 +0200

    Refactor checksum computing and add clarifying comments

--- 2024/day_9/solution.py
+++ 2024/day_9/solution.py
@@ -2,10 +2,20 @@ from utils_anviks import parse_file_content, stopwatch
 
 file = 'data.txt'
 file0 = 'example.txt'
-data = parse_file_content(file0, ('',), int)
-# Example: [(2+1j), (3+0j), (3+2j), (3+0j), (1+3j), (3+0j), (3+4j), (1+0j), (2+5j), (1+0j), (4+6j), (1+0j), (4+7j), (1+0j), (3+8j), (1+0j), (4+9j), 0j, (2+10j)]
-data1 = [data[i] + (i % 2 == 0) * (i + 2) * .5j for i in range(len(data))]
+data = parse_file_content(file, ('',), int)
+# Example: [(2+1j), (3+0j), (3+2j), (3+0j), (1+3j), (3+0j), (3+4j), (1+0j), (2+5j), ...]
+data1 = [data[i - 2] + (1 - i % 2) * i * .5j for i in range(2, len(data) + 2)]
 data2 = data1.copy()
+
+
+def compute_checksum(files):
+    pos = checksum = 0
+    for n in files:
+        for _ in range(int(n.real)):
+            if n.imag != 0:
+                checksum += pos * (n.imag - 1)
+            pos += 1
+    return int(checksum)
 
 
 @stopwatch
@@ -14,27 +24,19 @@ def part1():
 
     while l < len(data1) and r > 0:
         assert data1[l].imag == 0
-        if data1[r].real <= data1[l].real:
-            data1[l] -= data1[r].real
-            data1.insert(l, data1[r])
-            data1.pop()
-            data1.pop()
-            r -= 1
-            l += 1
-        else:
-            data1[l] += data1[r].imag * 1j
-            data1[r] -= data1[l].real
-            l += 2
+        if data1[r].real <= data1[l].real:  # File fits in the free space
+            data1[l] -= data1[r].real  # Subtract size of the file from the size of free space
+            data1.insert(l, data1[r])  # Insert the file at the beginning of the free space
+            data1.pop()  # Remove the last element (always the file that was inserted)
+            data1.pop()  # Remove the last element (always free space, even if its size is 0)
+            r -= 1  # Move to the next file
+            l += 1  # Due to insertion, remaining free space is shifted to the right
+        else:  # File doesn't fit in the free space
+            data1[l] += data1[r].imag * 1j  # File at r takes up the entire free space at l, so just add the ID of file to it
+            data1[r] -= data1[l].real  # Subtract the size of the free space from the size of the file
+            l += 2  # There was no insertion, and free space was used up, so move to the next free space
 
-    pos = 0
-    checksum = 0
-
-    for n in data1:
-        for _ in range(int(n.real)):
-            checksum += pos * (n.imag - 1)
-            pos += 1
-
-    return int(checksum)
+    return compute_checksum(data1)
 
 
 @stopwatch
@@ -44,31 +46,25 @@ def part2():
     while r > 0:
         l = 1
 
+        # Find the next file from the right
         while data2[r].imag == 0:
             r -= 1
 
+        # Find the first free space, where the entire file can fit
         while l < len(data2) and not (data2[l].imag == 0 and data2[l].real >= data2[r].real):
             l += 1
 
         if l < len(data2) and l < r:
+            # Switch the file with the free space
             data2[l] -= data2[r].real
-            data2[r - 1] += data2[r].real
+            data2[r - 1] += data2[r].real  # In this part, free space preservation is necessary!
             data2.insert(l, data2.pop(r))
 
         r -= 1
 
-    pos = 0
-    checksum = 0
-
-    for n in data2:
-        for _ in range(int(n.real)):
-            if n.imag != 0:
-                checksum += pos * (n.imag - 1)
-            pos += 1
-
-    return int(checksum)
+    return compute_checksum(data2)
 
 
 if __name__ == '__main__':
-    print(part1())  # 15996424901065    |   0.037 seconds
-    print(part2())  # 6478232739671     |   36.76 seconds
+    print(part1())  # 6446899523367    |   0.037 seconds
+    print(part2())  # 6478232739671     |   33.69 seconds
