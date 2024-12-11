d9337d595cdaa0efc18dc1a854127e6ad5c7e84d
anviks <anviks@ttu.ee>
2024-12-11 10:37:16 +0200

    Solve 2024 day 11

---
+++ 2024/day_11/solution.py
@@ -0,0 +1,44 @@
+from utils_anviks import parse_file_content, stopwatch
+from collections import Counter
+
+file = 'data.txt'
+file0 = 'example.txt'
+data = parse_file_content(file, (' ',), int)
+
+
+def blink(c1: Counter):
+    c2 = Counter()
+
+    for stone, count in c1.items():
+        if stone == 0:
+            c2[1] += c1[0]
+        elif len(str(stone)) % 2 == 0:
+            s = str(stone)
+            s1, s2 = int(s[:len(s) // 2]), int(s[len(s) // 2:])
+            c2[s1] += c1[stone]
+            c2[s2] += c1[stone]
+        else:
+            c2[stone * 2024] += c1[stone]
+
+    return c2
+
+
+@stopwatch
+def part1():
+    c = Counter(data)
+    for _ in range(25):
+        c = blink(c)
+    return sum(c.values())
+
+
+@stopwatch
+def part2():
+    c = Counter(data)
+    for _ in range(75):
+        c = blink(c)
+    return sum(c.values())
+
+
+if __name__ == '__main__':
+    print(part1())
+    print(part2())
