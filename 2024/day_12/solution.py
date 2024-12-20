9a5142f6f8cf6531e30af1b33ac2125de28235c1
anviks <anviks@ttu.ee>
2024-12-20 11:26:21 +0200

    Solve 2024 day 12 part 2

--- 2024/day_12/solution.py
+++ 2024/day_12/solution.py
@@ -9,9 +9,10 @@ data = parse_file_content(file, ('\n', ''), str)
 grid = Grid(data)
 
 
-def flood_fill(cell: Cell, char: str, visited: set[Cell]):
+def flood_fill(cell: Cell, char: str, total_visited: set[Cell]):
     stack = [cell]
-    visited.add(cell)
+    total_visited.add(cell)
+    area_cells = {cell}
     area = perimeter = 0
 
     while stack:
@@ -19,14 +20,24 @@ def flood_fill(cell: Cell, char: str, visited: set[Cell]):
         area += 1
 
         for nb in c.neighbours('cardinal'):
-            if nb not in visited and nb in grid and grid[nb] == char:
-                visited.add(nb)
+            if nb not in total_visited and nb in grid and grid[nb] == char:
+                total_visited.add(nb)
+                area_cells.add(nb)
                 stack.append(nb)
             else:
                 if nb not in grid or grid[nb] != char:
                     perimeter += 1
 
-    return area, perimeter
+    edges = set()
+
+    for point in area_cells:
+        for d in 1, -1, 1j, -1j:
+            if point + d not in area_cells:
+                edges.add((point, d))
+
+    sides = len(edges - {(p + d * 1j, d) for p, d in edges})
+
+    return area, perimeter, sides
 
 
 @stopwatch
@@ -35,15 +46,22 @@ def part1():
     visited = set()
     for coord, value in grid.items():
         if coord not in visited:
-            area, perimeter = flood_fill(coord, value, visited)
+            area, perimeter, sides = flood_fill(coord, value, visited)
             total += area * perimeter
     return total
 
 
+@stopwatch
 def part2():
-    pass
+    total = 0
+    visited = set()
+    for coord, value in grid.items():
+        if coord not in visited:
+            area, perimeter, sides = flood_fill(coord, value, visited)
+            total += area * sides
+    return total
 
 
 if __name__ == '__main__':
     print(part1())  # 1434856   | 0.22 seconds
-    print(part2())
+    print(part2())  # 891106
