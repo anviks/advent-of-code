10c9c3f73be2f673fd3ada68a674ef5d9894d264
anviks <anviks@ttu.ee>
2024-12-24 01:23:09 +0200

    Solve 2024 day 14

---
+++ 2024/day_14/solution.py
@@ -0,0 +1,52 @@
+import math
+
+from utils_anviks import parse_file_content, stopwatch
+
+from coordinates import Cell
+
+file = 'data.txt'
+file0 = 'example.txt'
+data = [[list(map(int, lo[2:].split(','))) for lo in loc] for loc in parse_file_content(file, ('\n', ' '), str)]
+robots = [[Cell(*robot[0][::-1]), complex(*robot[1][::-1])] for robot in data]
+w, h = 101, 103
+
+
+def move():
+    for robot in robots:
+        robot[0] += robot[1]
+        robot[0].row %= h
+        robot[0].column %= w
+
+    cells = [robot[0] for robot in robots]
+    quadrants = [0] * 4
+
+    for cell in cells:
+        quadrants[0] += cell.row < h // 2 and cell.column < w // 2
+        quadrants[1] += cell.row < h // 2 and cell.column > w // 2
+        quadrants[2] += cell.row > h // 2 and cell.column < w // 2
+        quadrants[3] += cell.row > h // 2 and cell.column > w // 2
+
+    return math.prod(quadrants)
+
+
+@stopwatch
+def part1():
+    a = 0
+    for _ in range(100):
+        a = move()
+    return a
+
+
+@stopwatch
+def part2():
+    for i in range(10_000):
+        cells = [robot[0] for robot in data]
+        move()
+        if len(cells) == len(set(cells)):
+            return i
+    return None
+
+
+if __name__ == '__main__':
+    print(part1())  # 219512160 | 0.07 seconds
+    print(part2())  # 6298      | 5.05 seconds
