db37ba7b4efdc9af98d33eabda0a768b0dd72384
anviks <anviks@ttu.ee>
2024-12-27 01:46:26 +0200

    Solve 2024 day 15 part 2

--- 2024/day_15/solution.py
+++ 2024/day_15/solution.py
@@ -6,6 +6,11 @@ from grid import Grid
 file = 'data.txt'
 file0 = 'example.txt'
 map1, instructions = parse_file_content(file, ('\n\n',), str)
+map2 = (map1
+        .replace('#', '##')
+        .replace('O', '[]')
+        .replace('.', '..')
+        .replace('@', '@.'))
 moves = instructions.replace('\n', '')
 comp_moves = []
 count, prev = 1, moves[0]
@@ -46,9 +51,58 @@ def part1():
 
 @stopwatch
 def part2():
-    pass
+    def can_move(from_: Cell, d: complex):
+        new = from_ + d
+        new_val = grid[new]
+        if new_val in '[]':
+            if d.imag:
+                return can_move(new + d, d)
+            else:
+                if new_val == '[':
+                    pair = new.right
+                else:
+                    pair = new.left
+                return can_move(new, d) and can_move(pair, d)
+
+        return new_val == '.'
+
+    def move(from_: Cell, d: complex):
+        to = from_ + d
+        to_val = grid[to]
+
+        if to_val == '.':
+            grid[to], grid[from_] = grid[from_], grid[to]
+            return
+
+        if to_val == '#':
+            return
+
+        if (d == -1j and to_val == ']'
+                or d == 1j and to_val == '['):
+            move(to + d, d)
+            grid[to + d], grid[to], grid[from_] = grid[to], grid[from_], grid[to + d]
+        elif d.real:
+            if to_val == ']':
+                move(to, d)
+                move(to.left, d)
+                grid[to], grid[from_] = grid[from_], grid[to]
+            elif to_val == '[':
+                move(to, d)
+                move(to.right, d)
+                grid[to], grid[from_] = grid[from_], grid[to]
+
+
+    grid = Grid(parse_string(map2, ('\n', ''), str))
+    robot = grid.find_first('@')
+    for arrow in moves:
+        direction = directions[arrow]
+        if can_move(robot, direction):
+            move(robot, direction)
+            robot += direction
+
+    return sum(box.row * 100 + box.column for box in grid.find('['))
 
 
 if __name__ == '__main__':
     print(part1())  # 1398947   | 0.116 seconds
-    print(part2())
+    print(part2())  # 1397393   | 0.144 seconds
