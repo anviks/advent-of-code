91abb8be619dd81423c454a2b8a7e9832e9b7717
anviks <anviks@ttu.ee>
2024-12-27 02:18:48 +0200

    Unify part 1 and part 2 solutions

--- 2024/day_15/solution.py
+++ 2024/day_15/solution.py
@@ -12,41 +12,37 @@ map2 = (map1
         .replace('.', '..')
         .replace('@', '@.'))
 moves = instructions.replace('\n', '')
-comp_moves = []
-count, prev = 1, moves[0]
-for i in range(1, len(moves)):
-    if moves[i] == prev:
-        count += 1
-    else:
-        comp_moves.append((count, prev))
-        count, prev = 1, moves[i]
-comp_moves.append((count, prev))
 directions = {'^': -1, 'v': 1, '<': -1j, '>': 1j}
+
+
+def solve(grid: Grid[str], can_move_func, move_func, box_symbol: str):
+    robot = grid.find_first('@')
+    for arrow in moves:
+        direction = directions[arrow]
+        if can_move_func(robot, direction):
+            move_func(robot, direction)
+            robot += direction
+
+    return sum(box.row * 100 + box.column for box in grid.find(box_symbol))
 
 
 @stopwatch
 def part1():
-    grid = Grid(parse_string(map1, ('\n', ''), str))
-    robot = grid.find_first('@')
-    for n, arrow in comp_moves:
-        direction = directions[arrow]
-        peek = robot + direction
-        free, boxes = 0, 0
-        while (val := grid[peek]) != '#' and free < n:
-            if val == '.':
-                free += 1
-            elif val == 'O':
-                boxes += 1
-            peek += direction
-        if free > 0:
-            grid[robot] = '.'
-            grid[robot:robot + direction * (free + boxes)] = '.'
-            robot = robot + direction * free
-            if boxes:
-                grid[robot:robot + direction * boxes] = 'O'
-            grid[robot] = '@'
+    def can_move(from_: Cell, d: complex):
+        new = from_ + d
+        new_val = grid[new]
+        if new_val == 'O':
+            return can_move(new, d)
+        return new_val == '.'
 
-    return sum(box.row * 100 + box.column for box in grid.find('O'))
+    def move(from_: Cell, d: complex):
+        to = from_ + d
+        if grid[to] == 'O':
+            move(to, d)
+        grid[to], grid[from_] = grid[from_], grid[to]
+
+    grid = Grid(parse_string(map1, ('\n', ''), str))
+    return solve(grid, can_move, move, 'O')
 
 
 @stopwatch
@@ -74,35 +70,22 @@ def part2():
             grid[to], grid[from_] = grid[from_], grid[to]
             return
 
-        if to_val == '#':
-            return
-
         if (d == -1j and to_val == ']'
                 or d == 1j and to_val == '['):
             move(to + d, d)
             grid[to + d], grid[to], grid[from_] = grid[to], grid[from_], grid[to + d]
         elif d.real:
+            move(to, d)
             if to_val == ']':
-                move(to, d)
                 move(to.left, d)
-                grid[to], grid[from_] = grid[from_], grid[to]
             elif to_val == '[':
-                move(to, d)
                 move(to.right, d)
-                grid[to], grid[from_] = grid[from_], grid[to]
-
+            grid[to], grid[from_] = grid[from_], grid[to]
 
     grid = Grid(parse_string(map2, ('\n', ''), str))
-    robot = grid.find_first('@')
-    for arrow in moves:
-        direction = directions[arrow]
-        if can_move(robot, direction):
-            move(robot, direction)
-            robot += direction
-
-    return sum(box.row * 100 + box.column for box in grid.find('['))
+    return solve(grid, can_move, move, '[')
 
 
 if __name__ == '__main__':
-    print(part1())  # 1398947   | 0.116 seconds
+    print(part1())  # 1398947   | 0.123 seconds
     print(part2())  # 1397393   | 0.144 seconds
