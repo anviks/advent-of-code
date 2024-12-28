53a6a71f25401ea7a54bd244d2da37e06dde0900
anviks <anviks@ttu.ee>
2024-12-28 13:13:17 +0200

    Solve 2024 day 20 part 2

--- 2024/day_20/solution.py
+++ 2024/day_20/solution.py
@@ -32,16 +32,39 @@ def part1():
     acc = 0
 
     for (cell_a, cost_a), (cell_b, cost_b) in combinations(costs.items(), 2):
-        if cell_a.euclidean_distance(cell_b) == 2 and abs(cost_a - cost_b) - 2 >= 100:
+        if cell_a.manhattan_distance(cell_b) == 2 and abs(cost_a - cost_b) - 2 >= 100:
             acc += 1
 
     return acc
 
 
+@stopwatch
 def part2():
-    pass
+    start, end, direction = grid.find_first('S'), grid.find_first('E'), 1j
+    uid = count()
+    todo = []
+    costs: dict[Cell, int] = {}
+    heappush(todo, (0, next(uid), start, direction))
+    while todo:
+        cost, _, loc, d = heappop(todo)  # type: int, int, Cell, complex
+        costs[loc] = cost
+        if loc == end:
+            break
+        for dir_ in (d, d * 1j, d * -1j):
+            new_loc = loc + dir_
+            if grid[new_loc] != '#':
+                heappush(todo, (cost + 1, next(uid), new_loc, dir_))
+
+    acc = 0
+
+    for (cell_a, cost_a), (cell_b, cost_b) in combinations(costs.items(), 2):
+        cheating_len = cell_a.manhattan_distance(cell_b)
+        if 1 <= cheating_len <= 20 and abs(cost_a - cost_b) - cheating_len >= 100:
+            acc += 1
+
+    return acc
 
 
 if __name__ == '__main__':
-    print(part1())  # 1387  | 17.4 seconds
-    print(part2())
+    print(part1())  # 1387      | 7.99 seconds
+    print(part2())  # 1015092   | 9.22 seconds
