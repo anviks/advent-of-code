277670361f28985f70e609209c00030e02b17732
anviks <anviks@ttu.ee>
2024-12-28 14:11:30 +0200

    Combine part 1 and part 2 solutions

--- 2024/day_20/solution.py
+++ 2024/day_20/solution.py
@@ -11,60 +11,43 @@ file0 = 'example.txt'
 data = parse_file_content(file, ('\n', ''), str)
 grid = Grid(data)
 
+start, end, direction = grid.find_first('S'), grid.find_first('E'), 1j
+uid = count()
+todo = []
+costs: dict[Cell, int] = {}
+heappush(todo, (0, next(uid), start, direction))
+while todo:
+    cost, _, loc, d = heappop(todo)  # type: int, int, Cell, complex
+    costs[loc] = cost
+    if loc == end:
+        break
+    for dir_ in (d, d * 1j, d * -1j):
+        new_loc = loc + dir_
+        if grid[new_loc] != '#':
+            heappush(todo, (cost + 1, next(uid), new_loc, dir_))
+
+
+def solve(max_cheat: int):
+    possibilities = 0
+
+    for (cell_a, cost_a), (cell_b, cost_b) in combinations(costs.items(), 2):
+        distance = cell_a.manhattan_distance(cell_b)
+        if distance <= max_cheat and abs(cost_a - cost_b) - distance >= 100:
+            possibilities += 1
+
+    return possibilities
+
 
 @stopwatch
 def part1():
-    start, end, direction = grid.find_first('S'), grid.find_first('E'), 1j
-    uid = count()
-    todo = []
-    costs: dict[Cell, int] = {}
-    heappush(todo, (0, next(uid), start, direction))
-    while todo:
-        cost, _, loc, d = heappop(todo)  # type: int, int, Cell, complex
-        costs[loc] = cost
-        if loc == end:
-            break
-        for dir_ in (d, d * 1j, d * -1j):
-            new_loc = loc + dir_
-            if grid[new_loc] != '#':
-                heappush(todo, (cost + 1, next(uid), new_loc, dir_))
-
-    acc = 0
-
-    for (cell_a, cost_a), (cell_b, cost_b) in combinations(costs.items(), 2):
-        if cell_a.manhattan_distance(cell_b) == 2 and abs(cost_a - cost_b) - 2 >= 100:
-            acc += 1
-
-    return acc
+    return solve(2)
 
 
 @stopwatch
 def part2():
-    start, end, direction = grid.find_first('S'), grid.find_first('E'), 1j
-    uid = count()
-    todo = []
-    costs: dict[Cell, int] = {}
-    heappush(todo, (0, next(uid), start, direction))
-    while todo:
-        cost, _, loc, d = heappop(todo)  # type: int, int, Cell, complex
-        costs[loc] = cost
-        if loc == end:
-            break
-        for dir_ in (d, d * 1j, d * -1j):
-            new_loc = loc + dir_
-            if grid[new_loc] != '#':
-                heappush(todo, (cost + 1, next(uid), new_loc, dir_))
-
-    acc = 0
-
-    for (cell_a, cost_a), (cell_b, cost_b) in combinations(costs.items(), 2):
-        cheating_len = cell_a.manhattan_distance(cell_b)
-        if 1 <= cheating_len <= 20 and abs(cost_a - cost_b) - cheating_len >= 100:
-            acc += 1
-
-    return acc
+    return solve(20)
 
 
 if __name__ == '__main__':
     print(part1())  # 1387      | 7.99 seconds
-    print(part2())  # 1015092   | 9.22 seconds
+    print(part2())  # 1015092   | 8.10 seconds
