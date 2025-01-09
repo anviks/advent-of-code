580e207c5ceb99d31f6567866a26ed455629420b
anviks <anviks@ttu.ee>
2025-01-09 13:14:05 +0200

    Make part 1 faster

--- 2015/day_6/solution.py
+++ 2015/day_6/solution.py
@@ -1,31 +1,30 @@
 import re
 
-from utils_anviks import Cell, Grid, stopwatch
+from utils_anviks import stopwatch
 
 file = 'data.txt'
 file0 = 'example.txt'
 with open(file) as f:
     data = re.findall(r'^(.+) (\d+),(\d+) through (\d+),(\d+)$', f.read(), re.MULTILINE)
 data = [(cmd, int(ax), int(ay), int(bx), int(by)) for cmd, ax, ay, bx, by in data]
-grid = Grid.fill(1000, 1000, 0)
+grid = [[0 for _ in range(1000)] for _ in range(1000)]
 
 
 @stopwatch
 def part1():
     for cmd, ax, ay, bx, by in data:
-        if cmd == 'turn on':
-            grid[Cell(ax, ay):Cell(bx, by)] = 1
-        elif cmd == 'turn off':
-            grid[Cell(ax, ay):Cell(bx, by)] = 0
-        else:
-            assert cmd == 'toggle'
-            on = [c for c in grid.find(1) if ax <= c.row <= bx and ay <= c.column <= by]
-            off = [c for c in grid.find(0) if ax <= c.row <= bx and ay <= c.column <= by]
-            if on:
-                grid[on] = 0
-            if off:
-                grid[off] = 1
-    return len(list(grid.find(1)))
+        for i in range(ax, bx + 1):
+            for j in range(ay, by + 1):
+                if cmd == 'turn on':
+                    grid[i][j] = 1
+                elif cmd == 'turn off':
+                    grid[i][j] = 0
+                else:
+                    if grid[i][j] == 1:
+                        grid[i][j] = 0
+                    else:
+                        grid[i][j] = 1
+    return sum(sum(row) for row in grid)
 
 
 @stopwatch
@@ -34,5 +33,5 @@ def part2():
 
 
 if __name__ == '__main__':
-    print(part1())  # 569999    | 30 seconds
+    print(part1())  # 569999    | 1.52 seconds
     print(part2())
