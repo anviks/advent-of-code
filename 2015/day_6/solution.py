267047eeca91be075393bd6fe3e37936015288ba
anviks <anviks@ttu.ee>
2025-01-09 13:18:51 +0200

    Solve 2025 day 6 part 2

--- 2015/day_6/solution.py
+++ 2015/day_6/solution.py
@@ -7,11 +7,11 @@ file0 = 'example.txt'
 with open(file) as f:
     data = re.findall(r'^(.+) (\d+),(\d+) through (\d+),(\d+)$', f.read(), re.MULTILINE)
 data = [(cmd, int(ax), int(ay), int(bx), int(by)) for cmd, ax, ay, bx, by in data]
-grid = [[0 for _ in range(1000)] for _ in range(1000)]
 
 
 @stopwatch
 def part1():
+    grid = [[0 for _ in range(1000)] for _ in range(1000)]
     for cmd, ax, ay, bx, by in data:
         for i in range(ax, bx + 1):
             for j in range(ay, by + 1):
@@ -29,9 +29,19 @@ def part1():
 
 @stopwatch
 def part2():
-    pass
+    grid = [[0 for _ in range(1000)] for _ in range(1000)]
+    for cmd, ax, ay, bx, by in data:
+        for i in range(ax, bx + 1):
+            for j in range(ay, by + 1):
+                if cmd == 'turn on':
+                    grid[i][j] += 1
+                elif cmd == 'turn off':
+                    grid[i][j] = max(0, grid[i][j] - 1)
+                else:
+                    grid[i][j] += 2
+    return sum(sum(row) for row in grid)
 
 
 if __name__ == '__main__':
     print(part1())  # 569999    | 1.52 seconds
-    print(part2())
+    print(part2())  # 18171238  | 1.97 seconds
