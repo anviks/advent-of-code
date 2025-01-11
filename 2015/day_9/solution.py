f66f0192ddf7e4af546e22a8869392ccda0ac5d6
anviks <anviks@ttu.ee>
2025-01-11 02:14:22 +0200

    Solve 2015 day 9

---
+++ 2015/day_9/solution.py
@@ -0,0 +1,42 @@
+from itertools import permutations
+
+from utils_anviks import parse_file_content, stopwatch
+
+file = 'data.txt'
+file0 = 'example.txt'
+data = parse_file_content(file, ('\n', ' = '), str)
+distances = {}
+cities = set()
+
+for cit, dist in data:
+    a, b = cit.split(' to ')
+    distances[a, b] = distances[b, a] = int(dist)
+    cities.add(a)
+    cities.add(b)
+
+
+@stopwatch
+def part1():
+    best = float('inf')
+    for perm in permutations(cities):
+        dis = 0
+        for pair in zip(perm, perm[1:]):
+            dis += distances[pair]
+        best = min(best, dis)
+    return best
+
+
+@stopwatch
+def part2():
+    best = float('-inf')
+    for perm in permutations(cities):
+        dis = 0
+        for pair in zip(perm, perm[1:]):
+            dis += distances[pair]
+        best = max(best, dis)
+    return best
+
+
+if __name__ == '__main__':
+    print(part1())  # 117   | 0.043 seconds
+    print(part2())  # 909   | 0.043 seconds
