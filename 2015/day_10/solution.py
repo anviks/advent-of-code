69f3f3219fde8d120f4af5a2088c791de10e9eba
anviks <anviks@ttu.ee>
2025-01-11 12:55:17 +0200

    Use one for-loop instead of two while-loops

--- 2015/day_10/solution.py
+++ 2015/day_10/solution.py
@@ -9,12 +9,12 @@ data = parse_file_content(file, (), str)
 
 def get_chunks(digits: str):
     chunks = []
-    l = r = 0
-    while r < len(digits):
-        while r < len(digits) and digits[r] == digits[l]:
-            r += 1
-        chunks.append((r - l, digits[l]))
-        l = r
+    l = 0
+    for r in range(1, len(digits)):
+        if digits[r] != digits[l]:
+            chunks.append((r - l, digits[l]))
+            l = r
+    chunks.append((len(digits) - l, digits[l]))
     return chunks
 
 
@@ -45,5 +45,5 @@ def part2():
 
 
 if __name__ == '__main__':
-    print(part1())  # 492982    | 0.51 seconds
-    print(part2())  # 6989950   | 7.17 seconds
+    print(part1())  # 492982    | 0.32 seconds
+    print(part2())  # 6989950   | 4.35 seconds
