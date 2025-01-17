8e767c38c52a158787798af5db2eb635bb143ed5
anviks <anviks@ttu.ee>
2025-01-17 14:15:21 +0200

    Make part 1 10x more efficient

--- 2015/day_17/solution.py
+++ 2015/day_17/solution.py
@@ -5,14 +5,20 @@ from utils_anviks import parse_file_content, stopwatch
 file = 'data.txt'
 file0 = 'example.txt'
 data = parse_file_content(file, ('\n',), int)
+data.sort(reverse=True)
 
 
 @stopwatch
 def part1():
-    result = 0
-    for r in range(1, len(data) + 1):
-        result += sum(1 for combo in combinations(data, r) if sum(combo) == 150)
-    return result
+    def find_combinations(idx, current_sum):
+        if current_sum == 150: return 1
+        if current_sum > 150 or idx == len(data): return 0
+
+        include = find_combinations(idx + 1, current_sum + data[idx])
+        exclude = find_combinations(idx + 1, current_sum)
+        return include + exclude
+
+    return find_combinations(0, 0)
 
 
 @stopwatch
@@ -32,5 +38,5 @@ def part2():
 
 
 if __name__ == '__main__':
-    print(part1())  # 1638  | 0.165 seconds
+    print(part1())  # 1638  | 0.0143 seconds
     print(part2())  # 17    | 0.00063 seconds
