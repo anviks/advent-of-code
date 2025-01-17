1d73ee487077a785754e9d9a8cabab0aa751013f
anviks <anviks@ttu.ee>
2025-01-17 13:35:31 +0200

    Solve 2015 day 17

---
+++ 2015/day_17/solution.py
@@ -0,0 +1,38 @@
+from itertools import combinations
+
+from utils_anviks import parse_file_content, stopwatch
+
+file = 'data.txt'
+file0 = 'example.txt'
+data = parse_file_content(file, ('\n',), int)
+
+
+@stopwatch
+def part1():
+    result = 0
+    for r in range(1, len(data) + 1):
+        for combo in combinations(data, r):
+            if sum(combo) == 150:
+                result += 1
+    return result
+
+
+@stopwatch
+def part2():
+    containers = float('inf')
+    ways_to_fill = 0
+
+    for r in range(1, len(data) + 1):
+        if r > containers:
+            break
+        for combo in combinations(data, r):
+            if sum(combo) == 150:
+                containers = r
+                ways_to_fill += 1
+
+    return ways_to_fill
+
+
+if __name__ == '__main__':
+    print(part1())  # 1638  | 0.165 seconds
+    print(part2())  # 17    | 0.00063 seconds
