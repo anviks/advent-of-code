8b901ce161156ffb1f16fac87e7af18044b55ced
anviks <anviks@ttu.ee>
2025-01-18 14:00:30 +0200

    Cache neighbours and use grid with boolean values instead of strings

--- 2015/day_18/solution.py
+++ 2015/day_18/solution.py
@@ -8,20 +8,23 @@ data = parse_file_content(file, ('\n', ''), str)
 @stopwatch
 def part1():
     grid = Grid(data)
+    grid = grid.map(lambda _, val: val == '#')
+    neighbour_cache = {cell: list(grid.neighbours(cell, 'all')) for cell, v in grid.items()}
+
     for _ in range(100):
         grid_copy = grid.copy()
         for cell, v in grid_copy.items():
             on_neighbours = 0
-            for nb in grid_copy.neighbours(cell, 'all'):
-                if grid_copy[nb] == '#':
+            for nb in neighbour_cache[cell]:
+                if grid_copy[nb]:
                     on_neighbours += 1
-            if v == '#':
+            if v:
                 if on_neighbours < 2 or on_neighbours > 3:
-                    grid[cell] = '.'
-            else:  # '.'
+                    grid[cell] = False
+            else:
                 if on_neighbours == 3:
-                    grid[cell] = '#'
-    return len(list(grid.find('#')))
+                    grid[cell] = True
+    return len(list(grid.find(True)))
 
 
 @stopwatch
@@ -47,5 +50,5 @@ def part2():
 
 
 if __name__ == '__main__':
-    print(part1())  # 768   | 13.88 seconds
-    print(part2())  # 781   | 15.20 seconds
+    print(part1())  # 768   | 5.9 seconds
+    # print(part2())  # 781   | 15.20 seconds
