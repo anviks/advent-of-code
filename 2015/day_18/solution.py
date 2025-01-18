d84a10f2ed5111e70fdf8f4b496416b38d0b5bea
anviks <anviks@ttu.ee>
2025-01-18 15:13:57 +0200

    Use scipy and numpy to improve performance by more than 15x

--- 2015/day_18/solution.py
+++ 2015/day_18/solution.py
@@ -1,4 +1,6 @@
-from utils_anviks import parse_file_content, stopwatch, Grid, Cell
+import numpy as np
+from scipy.signal import convolve2d
+from utils_anviks import Cell, Grid, parse_file_content, stopwatch
 
 file = 'data.txt'
 file0 = 'example.txt'
@@ -7,55 +9,33 @@ data = parse_file_content(file, ('\n', ''), str)
 
 @stopwatch
 def part1():
-    grid = Grid(data)
-    grid = grid.map(lambda _, val: val == '#')
-    neighbour_cache = {cell: list(grid.neighbours(cell, 'all')) for cell, v in grid.items()}
+    grid = np.array([[cell == '#' for cell in row] for row in data])
+    kernel = np.array([[1, 1, 1],
+                       [1, 0, 1],
+                       [1, 1, 1]])
 
     for _ in range(100):
-        turn_on = []
-        turn_off = []
+        neighbor_counts = convolve2d(grid, kernel, mode='same', boundary='fill', fillvalue=0)
+        grid = (grid & ((neighbor_counts == 2) | (neighbor_counts == 3))) | (~grid & (neighbor_counts == 3))
 
-        for cell, v in grid.items():
-            on_neighbours = 0
-            for nb in neighbour_cache[cell]:
-                if grid[nb]:
-                    on_neighbours += 1
-
-            if v:
-                if on_neighbours < 2 or on_neighbours > 3:
-                    turn_off.append(cell)
-            else:
-                if on_neighbours == 3:
-                    turn_on.append(cell)
-
-        grid[turn_on] = True
-        grid[turn_off] = False
-
-    return len(list(grid.find(True)))
+    return np.sum(grid)
 
 
 @stopwatch
 def part2():
-    grid = Grid(data)
+    grid = np.array([[cell == '#' for cell in row] for row in data])
+    kernel = np.array([[1, 1, 1],
+                       [1, 0, 1],
+                       [1, 1, 1]])
+
     for _ in range(100):
-        grid_copy = grid.copy()
-        for cell, v in grid_copy.items():
-            if cell in [Cell(0, 0), Cell(0, grid.width - 1),
-                        Cell(grid.height - 1, 0), Cell(grid.height - 1, grid.width - 1)]:
-                continue
-            on_neighbours = 0
-            for nb in grid_copy.neighbours(cell, 'all'):
-                if grid_copy[nb] == '#':
-                    on_neighbours += 1
-            if v == '#':
-                if on_neighbours < 2 or on_neighbours > 3:
-                    grid[cell] = '.'
-            else:  # '.'
-                if on_neighbours == 3:
-                    grid[cell] = '#'
-    return len(list(grid.find('#')))
+        neighbor_counts = convolve2d(grid, kernel, mode='same', boundary='fill', fillvalue=0)
+        grid = (grid & ((neighbor_counts == 2) | (neighbor_counts == 3))) | (~grid & (neighbor_counts == 3))
+        grid[::99, ::99] = 1
+
+    return np.sum(grid)
 
 
 if __name__ == '__main__':
-    print(part1())  # 768   | 5.86 seconds
-    # print(part2())  # 781   | 15.20 seconds
+    print(part1())  # 768   | 0.31 seconds
+    print(part2())  # 781   | 0.31 seconds
