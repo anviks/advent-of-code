acdb792e77a06676245cc57b44fcc1d2ba7495aa
anviks <anviks@ttu.ee>
2025-01-18 15:16:15 +0200

    Combine solutions

--- 2015/day_18/solution.py
+++ 2015/day_18/solution.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.signal import convolve2d
-from utils_anviks import Cell, Grid, parse_file_content, stopwatch
+from utils_anviks import parse_file_content, stopwatch
 
 file = 'data.txt'
 file0 = 'example.txt'
@@ -8,7 +8,7 @@ data = parse_file_content(file, ('\n', ''), str)
 
 
 @stopwatch
-def part1():
+def solution(part: int):
     grid = np.array([[cell == '#' for cell in row] for row in data])
     kernel = np.array([[1, 1, 1],
                        [1, 0, 1],
@@ -17,25 +17,12 @@ def part1():
     for _ in range(100):
         neighbor_counts = convolve2d(grid, kernel, mode='same', boundary='fill', fillvalue=0)
         grid = (grid & ((neighbor_counts == 2) | (neighbor_counts == 3))) | (~grid & (neighbor_counts == 3))
-
-    return np.sum(grid)
-
-
-@stopwatch
-def part2():
-    grid = np.array([[cell == '#' for cell in row] for row in data])
-    kernel = np.array([[1, 1, 1],
-                       [1, 0, 1],
-                       [1, 1, 1]])
-
-    for _ in range(100):
-        neighbor_counts = convolve2d(grid, kernel, mode='same', boundary='fill', fillvalue=0)
-        grid = (grid & ((neighbor_counts == 2) | (neighbor_counts == 3))) | (~grid & (neighbor_counts == 3))
-        grid[::99, ::99] = 1
+        if part == 2:
+            grid[::99, ::99] = 1
 
     return np.sum(grid)
 
 
 if __name__ == '__main__':
-    print(part1())  # 768   | 0.31 seconds
-    print(part2())  # 781   | 0.31 seconds
+    print(solution(1))  # 768   | 0.31 seconds
+    print(solution(2))  # 781   | 0.31 seconds
