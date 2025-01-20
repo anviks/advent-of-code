0d7514796df86a88346b64b4bde062e6b4fd5851
anviks <anviks@ttu.ee>
2025-01-20 13:10:27 +0200

    Return early for better performance

--- 2015/day_20/solution.py
+++ 2015/day_20/solution.py
@@ -11,7 +11,8 @@ def part1():
     presents = np.zeros(data // 10, dtype=int)
     for i in range(1, data // 10):
         presents[i::i] += i * 10
-    return np.where(presents >= data)[0].min()
+        if presents[i] >= data:
+            return i
 
 
 @stopwatch
@@ -19,9 +20,10 @@ def part2():
     presents = np.zeros(data // 10, dtype=int)
     for i in range(1, data // 10):
         presents[i:i*50:i] += i * 11
-    return np.where(presents >= data)[0].min()
+        if presents[i] >= data:
+            return i
 
 
 if __name__ == '__main__':
-    print(part1())  # 776160    | 7.5 seconds
-    print(part2())  # 786240    | 7.4 seconds
+    print(part1())  # 776160    | 1.76 seconds
+    print(part2())  # 786240    | 1.59 seconds
