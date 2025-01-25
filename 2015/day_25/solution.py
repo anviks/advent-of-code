377eece93b75eb3229b5fff2e4f46b55e3f73286
anviks <anviks@ttu.ee>
2025-01-26 01:55:01 +0200

    Simplify n calculation

--- 2015/day_25/solution.py
+++ 2015/day_25/solution.py
@@ -5,9 +5,7 @@ row, column = 2978, 3083
 
 @stopwatch
 def part1():
-    n = sum(range(column + 1))
-    for i in range(row - 1):
-        n += column + i
+    n = sum(range(row + column)) - row + 1
     code = 20151125
     for _ in range(n - 1):
         code = code * 252533 % 33554393
