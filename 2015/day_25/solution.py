1558aff9d0546cc571628cc7dddbe9f65c6f507e
anviks <anviks@ttu.ee>
2025-01-26 01:43:36 +0200

    Remove function call overhead

--- 2015/day_25/solution.py
+++ 2015/day_25/solution.py
@@ -3,10 +3,6 @@ from utils_anviks import stopwatch
 row, column = 2978, 3083
 
 
-def next_code(code: int):
-    return code * 252533 % 33554393
-
-
 @stopwatch
 def part1():
     n = sum(range(column + 1))
@@ -14,9 +10,9 @@ def part1():
         n += column + i
     code = 20151125
     for _ in range(n - 1):
-        code = next_code(code)
+        code = code * 252533 % 33554393
     return code
 
 
 if __name__ == '__main__':
-    print(part1())  # 2650453   | 2.41 seconds
+    print(part1())  # 2650453   | 1.30 seconds
