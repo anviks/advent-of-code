62a068f9aae225a9f6efae063acff8797b3e481d
anviks <anviks@ttu.ee>
2025-01-26 01:15:07 +0200

    Solve 2015 day 24

---
+++ 2015/day_24/solution.py
@@ -0,0 +1,24 @@
+import math
+from itertools import combinations
+
+from utils_anviks import parse_file_content, stopwatch
+
+file = 'data.txt'
+file0 = 'example.txt'
+data = parse_file_content(file, ('\n',), int)
+
+
+@stopwatch
+def solve(part: int):
+    group_weight = sum(data) // (2 + part)
+    for i in range(2, len(data)):
+        prods = []
+        for combo in (c for c in combinations(data, i) if sum(c) == group_weight):
+            prods.append(math.prod(combo))
+        if prods:
+            return min(prods)
+
+
+if __name__ == '__main__':
+    print(solve(1))  # 11846773891  | 0.079 seconds
+    print(solve(2))  # 80393059     | 0.003 seconds
