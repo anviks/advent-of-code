547290462fae0d486044979039b4a3037a4c6c36
Andreas Viks <117015417+anviks@users.noreply.github.com>
2025-10-11 13:54:19 +0300

    Solve 2016 day 6

---
+++ 2016/day_6/solution.py
@@ -0,0 +1,23 @@
+from collections import Counter
+from pathlib import Path
+from utils_anviks import parse_file_content, stopwatch
+
+file = "data.txt"
+file0 = "example.txt"
+file_path = str(Path(__file__).parent / file)
+data = parse_file_content(file_path, ("\n", ""), str)
+
+
+@stopwatch
+def part1():
+    return "".join(Counter(col).most_common()[0][0] for col in zip(*data))
+
+
+@stopwatch
+def part2():
+    return "".join(Counter(col).most_common()[-1][0] for col in zip(*data))
+
+
+if __name__ == "__main__":
+    print(part1())  # bjosfbce  | 0.00042 seconds
+    print(part2())  # veqfxzfx  | 0.00019 seconds
