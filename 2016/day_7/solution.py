06388b939b66f524fe8645cbbfc3f5279fa2e573
Andreas Viks <117015417+anviks@users.noreply.github.com>
2025-10-14 12:49:12 +0300

    Improve code and use more_itertools

--- 2016/day_7/solution.py
+++ 2016/day_7/solution.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import re
+from more_itertools import sliding_window
 from utils_anviks import parse_file_content, stopwatch
-from itertools import product, permutations
+from itertools import permutations
 from string import ascii_lowercase
 
 file = "data.txt"
@@ -11,9 +12,9 @@ data = parse_file_content(file_path, ("\n", re.compile(r"[\[\]]")), str)
 
 
 def has_abba(s: str) -> bool:
-    for i in range(len(s) - 3):
-        sub = s[i : i + 4]
-        if sub[0] != sub[1] and sub[:2][::-1] == sub[2:]:
+    for sub in sliding_window(s, 4):
+        a, b, _, _ = sub
+        if a != b and (a, b, b, a) == sub:
             return True
 
     return False
@@ -29,11 +30,7 @@ def part1():
 @stopwatch
 def part2():
     ips = 0
-    abas = []
-
-    for pair in permutations(ascii_lowercase, 2):
-        aba = pair[0] + pair[1] + pair[0]
-        abas.append(aba)
+    abas = [a + b + a for a, b in permutations(ascii_lowercase, 2)]
 
     for ip in data:
         for aba in abas:
@@ -49,5 +46,5 @@ def part2():
 
 
 if __name__ == "__main__":
-    print(part1())  # 118   | 0.021 seconds
+    print(part1())  # 118   | 0.013 seconds
     print(part2())  # 260   | 0.22 seconds
