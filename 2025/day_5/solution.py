9fc939a6b2c43f4c932bb6b4d9654498e000f679
Andreas Viks <117015417+anviks@users.noreply.github.com>
2025-12-06 01:52:56 +0200

    Remove range object usage

--- 2025/day_5/solution.py
+++ 2025/day_5/solution.py
@@ -5,9 +5,7 @@ file = "data.txt"
 file0 = "example.txt"
 file_path = Path(__file__).parent / file
 data = parse_file_content(file_path, ("\n\n",), str)
-ranges = [
-    range(start, end + 1) for start, end in parse_string(data[0], ("\n", "-"), int)
-]
+ranges = parse_string(data[0], ("\n", "-"), int)
 available = parse_string(data[1], ("\n",), int)
 
 
@@ -16,8 +14,8 @@ def part1():
     fresh = 0
 
     for ingredient in available:
-        for rang in ranges:
-            if ingredient in rang:
+        for start, stop in ranges:
+            if start <= ingredient <= stop:
                 fresh += 1
                 break
 
@@ -26,28 +24,23 @@ def part1():
 
 @stopwatch
 def part2():
-    sorted_ranges = sorted(ranges, key=lambda x: (x.start, x.stop))
-
+    sorted_ranges = sorted(ranges)
     i = 0
 
     while i < len(sorted_ranges) - 1:
-        if sorted_ranges[i].stop >= sorted_ranges[i + 1].start:
-            sorted_ranges[i] = range(
-                sorted_ranges[i].start,
-                max(sorted_ranges[i].stop, sorted_ranges[i + 1].stop),
-            )
+        # Connect overlapping pairs and pairs like [11, 20] and [21, 30]
+        if sorted_ranges[i][1] + 1 >= sorted_ranges[i + 1][0]:
+            sorted_ranges[i] = [
+                sorted_ranges[i][0],
+                max(sorted_ranges[i][1], sorted_ranges[i + 1][1]),
+            ]
             sorted_ranges.pop(i + 1)
         else:
             i += 1
 
-    fresh = 0
-
-    for rang in sorted_ranges:
-        fresh += rang.stop - rang.start
-
-    return fresh
+    return sum(stop - start + 1 for start, stop in sorted_ranges)
 
 
 if __name__ == "__main__":
-    print(part1())  # 707               | 0.0045 seconds
-    print(part2())  # 361615643045059   | 0.0001 seconds
+    print(part1())  # 707               | 0.0038 seconds
+    print(part2())  # 361615643045059   | 0.00009 seconds
