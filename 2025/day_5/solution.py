e2f7bc8f180d5028f8e0bd7497779d750cfda370
Andreas Viks <117015417+anviks@users.noreply.github.com>
2025-12-06 02:02:27 +0200

    Optimize ranges before everything

--- 2025/day_5/solution.py
+++ 2025/day_5/solution.py
@@ -5,8 +5,24 @@ file = "data.txt"
 file0 = "example.txt"
 file_path = Path(__file__).parent / file
 data = parse_file_content(file_path, ("\n\n",), str)
-ranges = parse_string(data[0], ("\n", "-"), int)
+ranges = sorted(parse_string(data[0], ("\n", "-"), int))
 available = parse_string(data[1], ("\n",), int)
+
+
+@stopwatch
+def optimize_ranges():
+    i = 0
+
+    while i < len(ranges) - 1:
+        # Connect overlapping pairs and pairs like [11, 20] and [21, 30]
+        if ranges[i][1] + 1 >= ranges[i + 1][0]:
+            ranges[i] = [
+                ranges[i][0],
+                max(ranges[i][1], ranges[i + 1][1]),
+            ]
+            ranges.pop(i + 1)
+        else:
+            i += 1
 
 
 @stopwatch
@@ -24,23 +40,10 @@ def part1():
 
 @stopwatch
 def part2():
-    sorted_ranges = sorted(ranges)
-    i = 0
-
-    while i < len(sorted_ranges) - 1:
-        # Connect overlapping pairs and pairs like [11, 20] and [21, 30]
-        if sorted_ranges[i][1] + 1 >= sorted_ranges[i + 1][0]:
-            sorted_ranges[i] = [
-                sorted_ranges[i][0],
-                max(sorted_ranges[i][1], sorted_ranges[i + 1][1]),
-            ]
-            sorted_ranges.pop(i + 1)
-        else:
-            i += 1
-
-    return sum(stop - start + 1 for start, stop in sorted_ranges)
+    return sum(stop - start + 1 for start, stop in ranges)
 
 
 if __name__ == "__main__":
-    print(part1())  # 707               | 0.0038 seconds
-    print(part2())  # 361615643045059   | 0.00009 seconds
+    optimize_ranges()  #                | 0.00004 seconds
+    print(part1())  # 707               | 0.0021 seconds
+    print(part2())  # 361615643045059   | 0.00001 seconds
