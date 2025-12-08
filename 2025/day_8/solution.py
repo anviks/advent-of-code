5f15694633ee704f11debe9ed3c246a52d07ad45
Andreas Viks <117015417+anviks@users.noreply.github.com>
2025-12-08 21:47:47 +0200

    Solve day 8 pt 2

--- 2025/day_8/solution.py
+++ 2025/day_8/solution.py
@@ -38,18 +38,51 @@ def part1():
                 box2_circ.add(box1)
             case _, _:
                 box1_circ.update(box2_circ)
-                box2_circ.clear()
+                circuits.remove(box2_circ)
 
         counter += 1
         if counter == 1000:
             break
 
-    return prod(sorted(map(len, filter(None, circuits)), reverse=True)[:3])
+    return prod(sorted(map(len, circuits), reverse=True)[:3])
 
 
 @stopwatch
 def part2():
-    pass
+    box_pairs = list(combinations(data, 2))
+    box_pairs.sort(
+        key=lambda boxes: sqrt(sum((boxes[1][i] - boxes[0][i]) ** 2 for i in range(3)))
+    )
+    circuits: list[set[tuple[int, ...]]] = []
+
+    result = 0
+
+    for box1, box2 in box_pairs:
+        box1_circ = box2_circ = None
+        for circ in circuits:
+            if box1 in circ:
+                box1_circ = circ
+            elif box2 in circ:
+                box2_circ = circ
+
+        match box1_circ, box2_circ:
+            case None, None:
+                circuits.append({box1, box2})
+            case _, None:
+                box1_circ.add(box2)
+                result = box1[0] * box2[0]
+            case None, _:
+                box2_circ.add(box1)
+                result = box1[0] * box2[0]
+            case _, _:
+                result = box1[0] * box2[0]
+                box1_circ.update(box2_circ)
+                circuits.remove(box2_circ)
+
+        if len(circuits) == 1 and len(circuits[0]) == len(data):
+            break
+
+    return result
 
 
 if __name__ == "__main__":
