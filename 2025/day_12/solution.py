4715b074ae03ca5244b4fe51597908267a1f7c43
Andreas Viks <117015417+anviks@users.noreply.github.com>
2025-12-26 14:32:41 +0200

    I can't believe this works...

--- 2025/day_12/solution.py
+++ 2025/day_12/solution.py
@@ -1,155 +1,22 @@
-from functools import cache, wraps
 from pathlib import Path
+import re
 from utils_anviks import parse_file_content, stopwatch, parse_string
-from concurrent.futures import ThreadPoolExecutor, TimeoutError
-
-type Shape = frozenset[tuple[int, int]]
-type Board = list[list[bool]]
 
 file = "data.txt"
 file0 = "example.txt"
-file_path = Path(__file__).parent / file0
-*shapes, regions = parse_file_content(file_path, ("\n\n",), str)
-regions = parse_string(regions, ("\n", ": ", " "), str)[:-1]
-
-shape_coords = []
-for shape in shapes:
-    rows = shape.split("\n")[1:]
-    coord_set = set()
-
-    for i, row in enumerate(rows):
-        for j, col in enumerate(row):
-            if col == "#":
-                coord_set.add((i, j))
-
-    shape_coords.append(frozenset(coord_set))
-
-
-def print_shape(shape: Shape):
-    max_i = max(i for i, j in shape)
-    max_j = max(j for i, j in shape)
-    for i in range(max_i + 1):
-        row = ""
-        for j in range(max_j + 1):
-            if (i, j) in shape:
-                row += "#"
-            else:
-                row += "."
-        print(row)
-    print()
-
-
-def timeout(seconds=1, default=None):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            with ThreadPoolExecutor(max_workers=1) as executor:
-                future = executor.submit(func, *args, **kwargs)
-                try:
-                    return future.result(timeout=seconds)
-                except TimeoutError:
-                    return default
-        return wrapper
-    return decorator
-
-
-@cache
-def normalize(cells):
-    minx = min(x for x, _ in cells)
-    miny = min(y for _, y in cells)
-    return frozenset((x - minx, y - miny) for x, y in cells)
-
-
-@cache
-def rotate(shape: Shape) -> Shape:
-    return normalize(frozenset({(y, -x) for x, y in shape}))
-
-
-@cache
-def flip(shape: Shape) -> Shape:
-    return normalize(frozenset({(-x, y) for x, y in shape}))
-
-
-@cache
-def all_variants(shape: Shape):
-    variants: set[Shape] = set()
-    cur = shape
-    for _ in range(4):
-        cur = rotate(cur)
-        variants.add(cur)
-        variants.add(flip(cur))
-    return list(variants)
-
-
-def can_place(board: Board, shape: Shape, x0: int, y0: int):
-    H, W = len(board), len(board[0])
-    for x, y in shape:
-        x += x0
-        y += y0
-        if x < 0 or y < 0 or x >= H or y >= W:
-            return False
-        if board[x][y]:
-            return False
-    return True
-
-
-def place(board: Board, shape: Shape, x0: int, y0: int, value=True):
-    for x, y in shape:
-        board[x + x0][y + y0] = value
-
-
-# @timeout(1, default=False)
-def solve(board: Board, shapes: list[list[Shape]], i=0):
-    if i == len(shapes):
-        return True
-
-    H, W = len(board), len(board[0])
-
-    for shape in shapes[i]:
-        maxx = max(x for x, _ in shape)
-        maxy = max(y for _, y in shape)
-
-        for x in range(H - maxy):
-            for y in range(W - maxx):
-                if can_place(board, shape, x, y):
-                    place(board, shape, x, y, True)
-                    if solve(board, shapes, i + 1):
-                        return True
-                    place(board, shape, x, y, False)
-
-    return False
+file_path = Path(__file__).parent / file
+*_, regs = parse_file_content(file_path, ("\n\n",), str)
+regions = parse_string(regs, ("\n", re.compile(r"\D+")), int)
 
 
 @stopwatch
 def part1():
-    total = 0
-
-    for (size,), counts in regions:
-        W, H = map(int, size.split("x"))
-        board = [[False] * W for _ in range(H)]
-        shapes: list[list[Shape]] = []
-
-        for i, count in enumerate(counts):
-            count = int(count)
-            if count == 0:
-                continue
-
-            for _ in range(count):
-                shapes.append(all_variants(shape_coords[i]))
-
-        result = solve(board, shapes)
-        print(result)
-        total += result
-
-    return total
-
-
-
-@stopwatch
-def part2():
-    pass
+    """
+    Idea taken from https://www.reddit.com/r/adventofcode/comments/1pkje0o/comment/ntlkg9i
+    I can't believe this works...
+    """
+    return sum(w // 3 * h // 3 >= sum(n) for w, h, *n in regions)
 
 
 if __name__ == "__main__":
-    print(part1())
-    print(part2())
+    print(part1())  # 546   | 0.00027 seconds
